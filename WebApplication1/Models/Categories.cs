1645b7a8bb9cd228d3b39e2156bf10c14f043b98
admin <admin@LAPTOP-N5T79K3J>
2023-03-08 13:22:33 +0700

    Add project files.

---
+++ WebApplication1/Models/Categories.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using RequiredAttribute = Microsoft.Build.Framework.RequiredAttribute;
+
+namespace WebApplication1.Models
+{
+    public class Categories
+    {
+        public byte Id { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+    }
+}
